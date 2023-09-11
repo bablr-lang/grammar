a280340581846073caf47fd531e7f365f4aef5f2
Conrad Buck <conartist6@gmail.com>
2023-09-11 10:43:27 -0600

    Fix buildAliases missing symbol-typed aliases

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,1 @@
-module.exports = {
-  parserOptions: {
-    ecmaVersion: 2020,
-    sourceType: 'module',
-  },
-  rules: {
-    'no-undef': 'error',
-  },
-  globals: {
-    require: 'readonly',
-    module: 'readonly',
-    Map: 'readonly',
-    Set: 'readonly',
-    WeakMap: 'readonly',
-    Symbol: 'readonly',
-    process: 'readonly',
-  },
-};
+module.exports = require('@bablr/eslint-config-base');
